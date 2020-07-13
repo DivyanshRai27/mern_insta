10cbd57d64445da5672d06cac238491da04012cf
DivyanshRai27 <divyanshrai27@gmail.com>
2020-07-13 09:53:34 +0530

    middleware

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://divyanshrai:divyanshrai@cluster0.aud89.mongodb.net/test?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://divyanshrai:divyanshrai@cluster0.aud89.mongodb.net/test?retryWrites=true&w=majority",
+    JWT_SECRET:"sdferguoihogfrljsdf"
 }
--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,15 @@ const router = express.Router()
 const mongoose = require("mongoose")
 const User = mongoose.model("User")
 const bcrypt = require("bcryptjs")
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require("../keys")
+const requireLogin = require('../middleware/requireLogin')
 
 router.get('/', (req, res) => {
+    res.send("hello")
+})
+
+router.get('/protected', requireLogin, (req, res) =>{ 
     res.send("hello")
 })
 
@@ -39,5 +46,33 @@ router.post('/signup', (req, res) => {
         console.log(err)
     })
 })
+
+router.post('/signin', (req, res) => {
+    const {email, password} = req.body
+    if (!email || !password) {
+       return res.status(422).json({error:"please add email or password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser => {
+        if(!savedUser){
+           return res.status(422).json({error: "Invalid email or password"})
+        }
+        bcrypt.compare(password, savedUser.password)
+        .then(doMatch=>{
+            if (doMatch) {
+                // res.json({message:"successfully signed in"})
+                const token = jwt.sign({_id: savedUser._id}, JWT_SECRET)
+                res.json({token})
+            }
+            else {
+                return res.status(422).json({error: "Invalid email or password"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+})
+
 
 module.exports = router
